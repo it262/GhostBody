731d5e5d192a069818fed6edb67f60d78e6f0004
it262 <sasura516@yahoo.co.jp>
2019-12-21 00:04:41 +0900

    Create camera controller sample

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+  //カメラは常にプレイヤーの方向を向く（現段階）
+  void Update()
+  {
+    Camera.main.transform.LookAt(transform.position);
+  }
+
+  //衝突したColliderの親（カメラの位置）へカメラを移動させる
+  private void OnTriggerEnter(Collider other)
+  {
+    Camera.main.transform.position = other.transform.parent.transform.position;
+  }
+}
