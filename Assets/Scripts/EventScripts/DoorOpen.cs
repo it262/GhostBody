e2ce61b5d650f40b6464c0ee47e0b8017b9a288e
fluitore <59082599+fluitore@users.noreply.github.com>
2020-01-02 14:36:22 +0900

    ClickEvent

---
+++ Assets/Scripts/EventScripts/DoorOpen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    bool isOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DoorEvent()
+    {
+        if (!isOpen)
+        {
+            transform.Rotate(0, 90, 0);
+            isOpen = true;
+        }
+        else
+        {
+            transform.Rotate(0, -90, 0);
+            isOpen = false;
+        }
+    }
+}
---
+++ Assets/Scripts/EventScripts/TextEvent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextEvent : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    bool isClick;
+    void Start()
+    {
+        this.GetComponent<Canvas>().enabled = false;
+        isClick = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickTextEvent()
+    {
+        if (!isClick)
+        {
+            this.GetComponentInChildren<Text>().text = "これは四角です。もう一度クリックするとUIが消えます。";
+            this.GetComponent<Canvas>().enabled = true;
+            isClick = true;
+        }
+        else
+        {
+            this.GetComponent<Canvas>().enabled = false;
+            isClick = false;
+        }
+      
+    }
+}
